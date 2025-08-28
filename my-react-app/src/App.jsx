48d1b985c36dc665539f24d98535ed1dd64912db
ChristinaW44 <103203583+ChristinaW44@users.noreply.github.com>
2025-08-28 23:49:36 +0100

    added click event to button

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -8,24 +8,11 @@ import UserGreeting from "./UserGreeting";
 import List from "./List";
 
 function App() {
-  // const fruits = [{id: 1, name:"apple", calories: 95}, 
-  //                   {id: 2, name:"orange", calories: 45}, 
-  //                   {id: 3,name:"bannana", calories: 105}, 
-  //                   {id: 4,name:"coconut", calories: 159}, 
-  //                   {id: 5,name:"pineapple", calories: 37}];
-  const fruits = [];
-  
-  const vegtables = [{id: 6, name:"potatoes", calories: 110}, 
-                    {id: 7, name:"celery", calories: 15}, 
-                    {id: 8,name:"carrots", calories: 25}, 
-                    {id: 9,name:"corn", calories: 63}, 
-                    {id: 10,name:"broccoli", calories: 50}];
 
   return (
     <>
       <Header />
-      {fruits.length > 0 && <List  items={fruits} />}
-      {vegtables.length > 0 ? <List  items={vegtables} category="Vegtables"/> : null}
+      <Button />
       <Footer />
     </>    
   );
--- my-react-app/src/Button/Button.jsx
+++ my-react-app/src/Button/Button.jsx
@@ -1,8 +1,19 @@
 import styles from './Button.module.css'
 
 function Button(){
+    let count = 0;
+
+    const handleClick = (name) => {
+        if(count < 3){
+            count++;
+            console.log(`${name} you clicked me ${count} time/s`);
+        } else {
+            console.log(`${name} stop clicking me`);
+        }
+    };
+
     return(
-        <button className={styles.button}>Click me</button>
+        <button onClick={() => handleClick("MyName")} className={styles.button}>Click me</button>
     );
 }
 
