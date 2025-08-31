f4bf9366cae82377991de7cc5004df108cb90d7c
ChristinaW44 <103203583+ChristinaW44@users.noreply.github.com>
2025-08-31 23:55:32 +0100

    used on change methods on input fields and text area fields

--- my-react-app/src/MyComponent.jsx
+++ my-react-app/src/MyComponent.jsx
@@ -2,29 +2,51 @@ import React, {useState} from "react";
 
 function MyComponent(){
     const [name, setName] = useState("Guest");
-    const [age, setAge] = useState(0);
-    const [isEmployed, setIsEmployed] = useState(false);
+    const [quantity, setQuantity] = useState(1);
+    const [comment, setComment] = useState("");
+    // const [age, setAge] = useState(0);
+    // const [isEmployed, setIsEmployed] = useState(false);
 
-    const updateName = () => {
-        setName("Spongebob");
+    // const updateName = () => {
+    //     setName("Spongebob");
+    // }
+    // const incrementAge = () => {
+    //     setAge(age + 2);
+    // }
+    // const toggleEmployed = () => {
+    //     setIsEmployed(!isEmployed)
+    // }
+
+    function handleNameChange(event){
+        setName(event.target.value);
     }
-    const incrementAge = () => {
-        setAge(age + 2);
+    function handleQuantityChange(event){
+        setQuantity(event.target.value);
     }
-    const toggleEmployed = () => {
-        setIsEmployed(!isEmployed)
+    function handleCommentChange(event){
+        setComment(event.target.value);
     }
 
     return(
         <div>
-            <p>Name: {name}</p>
+            {/* <p>Name: {name}</p>
             <button onClick={updateName}>Set Name</button>
             <p>Age: {age}</p>
             <button onClick={incrementAge}>Increment Age</button>
             <p>Is Employed: {isEmployed ? "Yes" : "No"}</p>
-            <button onClick={toggleEmployed}>Toggle Employed status</button>
+            <button onClick={toggleEmployed}>Toggle Employed status</button> */}
+            <input value={name} onChange={handleNameChange}></input>
+            <p>Name: {name}</p>
+
+            <input value={quantity} onChange={handleQuantityChange} type="number"></input>
+            <p>Quantity: {quantity}</p>
+
+            <textarea value={comment} onChange={handleCommentChange} placeholder="Enter Delivery Instructions"></textarea>
+            <p>Comment: {comment}</p>
         </div>
     );
+    
+
 }
 
 export default MyComponent
