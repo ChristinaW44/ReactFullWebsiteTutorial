d7812d4bdd41a4fa59b289372f450c7ed3218ad7
ChristinaW44 <103203583+ChristinaW44@users.noreply.github.com>
2025-09-01 00:05:35 +0100

    used onChange methods on select and radio buttons

--- my-react-app/src/MyComponent.jsx
+++ my-react-app/src/MyComponent.jsx
@@ -4,6 +4,8 @@ function MyComponent(){
     const [name, setName] = useState("Guest");
     const [quantity, setQuantity] = useState(1);
     const [comment, setComment] = useState("");
+    const [payment, setPayment] = useState("");
+    const [shipping, setShipping] = useState("Delivery");
     // const [age, setAge] = useState(0);
     // const [isEmployed, setIsEmployed] = useState(false);
 
@@ -26,6 +28,12 @@ function MyComponent(){
     function handleCommentChange(event){
         setComment(event.target.value);
     }
+    function handlePaymentChange(event){
+        setPayment(event.target.value);
+    }
+    function handleShippingChange(event){
+        setShipping(event.target.value);
+    }
 
     return(
         <div>
@@ -43,6 +51,24 @@ function MyComponent(){
 
             <textarea value={comment} onChange={handleCommentChange} placeholder="Enter Delivery Instructions"></textarea>
             <p>Comment: {comment}</p>
+
+            <select value={payment} onChange={handlePaymentChange}>
+                <option value={""}>Select an option</option>
+                <option value={"Visa"}>Visa</option>
+                <option value={"Mastercard"}>Mastercard</option>
+                <option value={"Giftcard"}>Giftcard</option>
+            </select>
+            <p>Payment: {payment}</p>
+
+            <label>
+                <input type="radio" value={"Pick up"} checked={shipping==="Pick up"} onChange={handleShippingChange}/>
+                Pick up
+            </label>
+            <label>
+                <input type="radio" value={"Delivery"} checked={shipping==="Delivery"} onChange={handleShippingChange}/>
+                Delivery
+            </label>
+            <p>Shipping: {shipping}</p>
         </div>
     );
     
