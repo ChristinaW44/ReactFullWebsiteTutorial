27ed75f47cb548c0da58c037358162fa4ae808ae
ChristinaW44 <103203583+ChristinaW44@users.noreply.github.com>
2025-08-31 22:45:51 +0100

    using useStates to increment integers and toggle booleans

--- my-react-app/src/MyComponent.jsx
+++ my-react-app/src/MyComponent.jsx
@@ -2,15 +2,27 @@ import React, {useState} from "react";
 
 function MyComponent(){
     const [name, setName] = useState("Guest");
+    const [age, setAge] = useState(0);
+    const [isEmployed, setIsEmployed] = useState(false);
 
     const updateName = () => {
         setName("Spongebob");
+    }
+    const incrementAge = () => {
+        setAge(age + 2);
+    }
+    const toggleEmployed = () => {
+        setIsEmployed(!isEmployed)
     }
 
     return(
         <div>
             <p>Name: {name}</p>
             <button onClick={updateName}>Set Name</button>
+            <p>Age: {age}</p>
+            <button onClick={incrementAge}>Increment Age</button>
+            <p>Is Employed: {isEmployed ? "Yes" : "No"}</p>
+            <button onClick={toggleEmployed}>Toggle Employed status</button>
         </div>
     );
 }
