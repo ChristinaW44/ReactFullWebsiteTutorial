935e37ddc8156047ef76945c9c46771074f921ec
ChristinaW44 <103203583+ChristinaW44@users.noreply.github.com>
2025-09-03 00:08:28 +0100

    updated an object in state

--- my-react-app/src/MyComponent.jsx
+++ my-react-app/src/MyComponent.jsx
@@ -1,11 +1,11 @@
 import React, {useState} from "react";
 
 function MyComponent(){
-    const [name, setName] = useState("Guest");
-    const [quantity, setQuantity] = useState(1);
-    const [comment, setComment] = useState("");
-    const [payment, setPayment] = useState("");
-    const [shipping, setShipping] = useState("Delivery");
+    // const [name, setName] = useState("Guest");
+    // const [quantity, setQuantity] = useState(1);
+    // const [comment, setComment] = useState("");
+    // const [payment, setPayment] = useState("");
+    // const [shipping, setShipping] = useState("Delivery");
     // const [age, setAge] = useState(0);
     // const [isEmployed, setIsEmployed] = useState(false);
 
@@ -19,20 +19,32 @@ function MyComponent(){
     //     setIsEmployed(!isEmployed)
     // }
 
-    function handleNameChange(event){
-        setName(event.target.value);
+    // function handleNameChange(event){
+    //     setName(event.target.value);
+    // }
+    // function handleQuantityChange(event){
+    //     setQuantity(event.target.value);
+    // }
+    // function handleCommentChange(event){
+    //     setComment(event.target.value);
+    // }
+    // function handlePaymentChange(event){
+    //     setPayment(event.target.value);
+    // }
+    // function handleShippingChange(event){
+    //     setShipping(event.target.value);
+    // }
+
+    const [car,setCar] = useState({year:2024, make:"Ford", model:"Mustang"});
+
+    function handleYearChange(event){
+        setCar(c => ({...c, year: event.target.value}));
     }
-    function handleQuantityChange(event){
-        setQuantity(event.target.value);
+    function handleMakeChange(event){
+        setCar(c => ({...c, make: event.target.value}));
     }
-    function handleCommentChange(event){
-        setComment(event.target.value);
-    }
-    function handlePaymentChange(event){
-        setPayment(event.target.value);
-    }
-    function handleShippingChange(event){
-        setShipping(event.target.value);
+    function handleModelChange(event){
+        setCarc(c => ({...c, model: event.target.value}));
     }
 
     return(
@@ -43,7 +55,7 @@ function MyComponent(){
             <button onClick={incrementAge}>Increment Age</button>
             <p>Is Employed: {isEmployed ? "Yes" : "No"}</p>
             <button onClick={toggleEmployed}>Toggle Employed status</button> */}
-            <input value={name} onChange={handleNameChange}></input>
+            {/* <input value={name} onChange={handleNameChange}></input>
             <p>Name: {name}</p>
 
             <input value={quantity} onChange={handleQuantityChange} type="number"></input>
@@ -68,7 +80,14 @@ function MyComponent(){
                 <input type="radio" value={"Delivery"} checked={shipping==="Delivery"} onChange={handleShippingChange}/>
                 Delivery
             </label>
-            <p>Shipping: {shipping}</p>
+            <p>Shipping: {shipping}</p> */}
+
+            <p>Your favourite car is : {car.year} {car.make} {car.model}</p>
+
+            <input type="number" value={car.year} onChange={handleYearChange}/><br/>
+            <input type="text" value={car.make} onChange={handleMakeChange}/><br/>
+            <input type="text" value={car.model} onChange={handleModelChange}/><br/>
+
         </div>
     );
     
