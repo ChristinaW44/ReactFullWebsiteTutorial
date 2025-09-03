be45cd7960aedb6751a042fb77176962d49691a0
ChristinaW44 <103203583+ChristinaW44@users.noreply.github.com>
2025-09-03 21:19:27 +0100

    used useRef to avoid rerendering components

--- my-react-app/src/MyComponent.jsx
+++ my-react-app/src/MyComponent.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useRef} from "react";
 
 // function MyComponent(){
 //     // const [name, setName] = useState("Guest");
@@ -190,35 +190,81 @@ import React, {useState, useEffect} from "react";
 //     )
 // }
 
+// function MyComponent(){
+//     const [width, setWidth] = useState(window.innerWidth);
+//     const [height, setHeight] = useState(window.innerHeight);
+
+//     useEffect(() => {
+//         window.addEventListener("resize", handleResize);
+//         console.log("EVENT LISTENER ADDED");
+
+//         return () => {
+//             window.removeEventListener("resize", handleResize);
+//             console.log("EVENT LISTENER REMOVED");
+//         }
+//     }, []);
+
+//     useEffect(() => {
+//         document.title = `Size: ${width} x ${height}`;
+//     }, [width, height]);
+
+//     function handleResize(){
+//         setWidth(window.innerWidth);
+//         setHeight(window.innerHeight);
+//     }
+
+//     return(
+//         <>
+//             <p>Window Width: {width}px</p>
+//             <p>Window Height: {height}px</p>
+//         </>
+//     )
+// }
+
 function MyComponent(){
-    const [width, setWidth] = useState(window.innerWidth);
-    const [height, setHeight] = useState(window.innerHeight);
+    const inputRef1 = useRef(null);
+    const inputRef2 = useRef(null);
+    const inputRef3 = useRef(null);
 
-    useEffect(() => {
-        window.addEventListener("resize", handleResize);
-        console.log("EVENT LISTENER ADDED");
+    function handleClick1(){
+        inputRef1.current.focus();
+        inputRef1.current.style.backgroundColor = "yellow";
+        inputRef2.current.style.backgroundColor = "";
+        inputRef3.current.style.backgroundColor = "";
+    }
 
-        return () => {
-            window.removeEventListener("resize", handleResize);
-            console.log("EVENT LISTENER REMOVED");
-        }
-    }, []);
+    function handleClick2(){
+        inputRef2.current.focus();
+        inputRef1.current.style.backgroundColor = "";
+        inputRef2.current.style.backgroundColor = "yellow";
+        inputRef3.current.style.backgroundColor = "";
+    }
 
-    useEffect(() => {
-        document.title = `Size: ${width} x ${height}`;
-    }, [width, height]);
-
-    function handleResize(){
-        setWidth(window.innerWidth);
-        setHeight(window.innerHeight);
+    function handleClick3(){
+        inputRef3.current.focus();
+        inputRef1.current.style.backgroundColor = "";
+        inputRef2.current.style.backgroundColor = "";
+        inputRef3.current.style.backgroundColor = "yellow";
     }
 
     return(
-        <>
-            <p>Window Width: {width}px</p>
-            <p>Window Height: {height}px</p>
-        </>
-    )
+        <div>
+            <button onClick={handleClick1}>
+                Click me 1!
+            </button>
+            <input ref={inputRef1}/> <br/>
+
+            <button onClick={handleClick2}>
+                Click me 2!
+            </button>
+            <input ref={inputRef2}/><br/>
+
+            <button onClick={handleClick3}>
+                Click me 3!
+            </button>
+            <input ref={inputRef3}/><br/>
+        </div>        
+    );
 }
 
 export default MyComponent
