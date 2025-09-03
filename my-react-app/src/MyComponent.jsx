8131bfae49cf21ff86ac1c434fd72c56ad29588a
ChristinaW44 <103203583+ChristinaW44@users.noreply.github.com>
2025-09-03 16:37:17 +0100

    updating title using useEffect everytime count is re-rendered

--- my-react-app/src/MyComponent.jsx
+++ my-react-app/src/MyComponent.jsx
@@ -1,162 +1,181 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
+
+// function MyComponent(){
+//     // const [name, setName] = useState("Guest");
+//     // const [quantity, setQuantity] = useState(1);
+//     // const [comment, setComment] = useState("");
+//     // const [payment, setPayment] = useState("");
+//     // const [shipping, setShipping] = useState("Delivery");
+//     // const [age, setAge] = useState(0);
+//     // const [isEmployed, setIsEmployed] = useState(false);
+
+//     // const updateName = () => {
+//     //     setName("Spongebob");
+//     // }
+//     // const incrementAge = () => {
+//     //     setAge(age + 2);
+//     // }
+//     // const toggleEmployed = () => {
+//     //     setIsEmployed(!isEmployed)
+//     // }
+
+//     // function handleNameChange(event){
+//     //     setName(event.target.value);
+//     // }
+//     // function handleQuantityChange(event){
+//     //     setQuantity(event.target.value);
+//     // }
+//     // function handleCommentChange(event){
+//     //     setComment(event.target.value);
+//     // }
+//     // function handlePaymentChange(event){
+//     //     setPayment(event.target.value);
+//     // }
+//     // function handleShippingChange(event){
+//     //     setShipping(event.target.value);
+//     // }
+
+//     // const [car,setCar] = useState({year:2024, make:"Ford", model:"Mustang"});
+
+//     // function handleYearChange(event){
+//     //     setCar(c => ({...c, year: event.target.value}));
+//     // }
+//     // function handleMakeChange(event){
+//     //     setCar(c => ({...c, make: event.target.value}));
+//     // }
+//     // function handleModelChange(event){
+//     //     setCarc(c => ({...c, model: event.target.value}));
+//     // }
+
+//     // const [foods, setFoods] = useState(["Apple", "Orange", "Bannana"]);
+
+//     // function handleAddFood(){
+//     //     const newFood = document.getElementById("foodInput").value;
+//     //     document.getElementById("foodInput").value = "";
+
+//     //     setFoods(f => [...f, newFood]);
+//     // }
+
+//     // function handleRemoveFood(index){
+//     //     setFoods(foods.filter((_, i) => i !== index));
+//     // }
+
+//     const [cars, setCars] = useState([]);
+//     const [carYear, setCarYear] = useState(new Date().getFullYear());
+//     const [carMake, setCarMake] = useState("");
+//     const [carModel, setCarModel] = useState("");
+
+//     function handleAddCar(){
+//         const newCar = {year: carYear, make: carMake, model: carModel};
+//         setCars(c => [...c, newCar]);
+
+//         setCarYear(new Date().getFullYear());
+//         setCarMake("");
+//         setCarModel("");
+//     }
+
+//     function handleRemoveCar(index){
+//         setCars(c => c.filter((_, i) => i !== index))
+//     }
+
+//     function handleYearChange(event){
+//         setCarYear(event.target.value);
+//     }
+
+//     function handleMakeChange(event){
+//         setCarMake(event.target.value);
+        
+//     }
+//     function handleModelChange(event){
+//         setCarModel(event.target.value);
+        
+//     }
+
+//     return(
+//         <div>
+//             {/* <p>Name: {name}</p>
+//             <button onClick={updateName}>Set Name</button>
+//             <p>Age: {age}</p>
+//             <button onClick={incrementAge}>Increment Age</button>
+//             <p>Is Employed: {isEmployed ? "Yes" : "No"}</p>
+//             <button onClick={toggleEmployed}>Toggle Employed status</button> */}
+//             {/* <input value={name} onChange={handleNameChange}></input>
+//             <p>Name: {name}</p>
+
+//             <input value={quantity} onChange={handleQuantityChange} type="number"></input>
+//             <p>Quantity: {quantity}</p>
+
+//             <textarea value={comment} onChange={handleCommentChange} placeholder="Enter Delivery Instructions"></textarea>
+//             <p>Comment: {comment}</p>
+
+//             <select value={payment} onChange={handlePaymentChange}>
+//                 <option value={""}>Select an option</option>
+//                 <option value={"Visa"}>Visa</option>
+//                 <option value={"Mastercard"}>Mastercard</option>
+//                 <option value={"Giftcard"}>Giftcard</option>
+//             </select>
+//             <p>Payment: {payment}</p>
+
+//             <label>
+//                 <input type="radio" value={"Pick up"} checked={shipping==="Pick up"} onChange={handleShippingChange}/>
+//                 Pick up
+//             </label>
+//             <label>
+//                 <input type="radio" value={"Delivery"} checked={shipping==="Delivery"} onChange={handleShippingChange}/>
+//                 Delivery
+//             </label>
+//             <p>Shipping: {shipping}</p> */}
+
+//             {/* <p>Your favourite car is : {car.year} {car.make} {car.model}</p>
+
+//             <input type="number" value={car.year} onChange={handleYearChange}/><br/>
+//             <input type="text" value={car.make} onChange={handleMakeChange}/><br/>
+//             <input type="text" value={car.model} onChange={handleModelChange}/><br/> */}
+
+//             {/* <h2>List of food</h2>
+//             <ul>
+//                 {foods.map((food, index) => <li key={index} onClick={() => handleRemoveFood(index)}>{food}</li>)}
+//             </ul>
+
+//             <input type="text" id="foodInput" placeholder="Enter food name"/>
+//             <button onClick={handleAddFood}>Add food</button> */}
+
+//             <h2>List of Car Objects</h2>
+//             <ul>
+//                 {cars.map((car, index) => 
+//                 <li key={index} onClick={() => handleRemoveCar(index)}>
+//                     {car.year} {car.make} {car.model}
+//                 </li>)}
+//             </ul>
+
+//             <input type="number" value={carYear} onChange={handleYearChange}/><br />
+//             <input type="text" value={carMake} onChange={handleMakeChange} placeholder="Enter car make"/><br />
+//             <input type="text" value={carModel} onChange={handleModelChange} placeholder="Enter car model"/><br />
+//             <button onClick={handleAddCar}>Add Car</button>
+
+//         </div>
+//     );
+    
+
+// }
 
 function MyComponent(){
-    // const [name, setName] = useState("Guest");
-    // const [quantity, setQuantity] = useState(1);
-    // const [comment, setComment] = useState("");
-    // const [payment, setPayment] = useState("");
-    // const [shipping, setShipping] = useState("Delivery");
-    // const [age, setAge] = useState(0);
-    // const [isEmployed, setIsEmployed] = useState(false);
+    const [count, setCount] = useState(0);
 
-    // const updateName = () => {
-    //     setName("Spongebob");
-    // }
-    // const incrementAge = () => {
-    //     setAge(age + 2);
-    // }
-    // const toggleEmployed = () => {
-    //     setIsEmployed(!isEmployed)
-    // }
+    useEffect(() => {
+        document.title = `Count: ${count}`;
+    });
 
-    // function handleNameChange(event){
-    //     setName(event.target.value);
-    // }
-    // function handleQuantityChange(event){
-    //     setQuantity(event.target.value);
-    // }
-    // function handleCommentChange(event){
-    //     setComment(event.target.value);
-    // }
-    // function handlePaymentChange(event){
-    //     setPayment(event.target.value);
-    // }
-    // function handleShippingChange(event){
-    //     setShipping(event.target.value);
-    // }
-
-    // const [car,setCar] = useState({year:2024, make:"Ford", model:"Mustang"});
-
-    // function handleYearChange(event){
-    //     setCar(c => ({...c, year: event.target.value}));
-    // }
-    // function handleMakeChange(event){
-    //     setCar(c => ({...c, make: event.target.value}));
-    // }
-    // function handleModelChange(event){
-    //     setCarc(c => ({...c, model: event.target.value}));
-    // }
-
-    // const [foods, setFoods] = useState(["Apple", "Orange", "Bannana"]);
-
-    // function handleAddFood(){
-    //     const newFood = document.getElementById("foodInput").value;
-    //     document.getElementById("foodInput").value = "";
-
-    //     setFoods(f => [...f, newFood]);
-    // }
-
-    // function handleRemoveFood(index){
-    //     setFoods(foods.filter((_, i) => i !== index));
-    // }
-
-    const [cars, setCars] = useState([]);
-    const [carYear, setCarYear] = useState(new Date().getFullYear());
-    const [carMake, setCarMake] = useState("");
-    const [carModel, setCarModel] = useState("");
-
-    function handleAddCar(){
-        const newCar = {year: carYear, make: carMake, model: carModel};
-        setCars(c => [...c, newCar]);
-
-        setCarYear(new Date().getFullYear());
-        setCarMake("");
-        setCarModel("");
-    }
-
-    function handleRemoveCar(index){
-        setCars(c => c.filter((_, i) => i !== index))
-    }
-
-    function handleYearChange(event){
-        setCarYear(event.target.value);
-    }
-
-    function handleMakeChange(event){
-        setCarMake(event.target.value);
-        
-    }
-    function handleModelChange(event){
-        setCarModel(event.target.value);
-        
+    function addCount(){
+        setCount(c => c + 1);
     }
 
     return(
-        <div>
-            {/* <p>Name: {name}</p>
-            <button onClick={updateName}>Set Name</button>
-            <p>Age: {age}</p>
-            <button onClick={incrementAge}>Increment Age</button>
-            <p>Is Employed: {isEmployed ? "Yes" : "No"}</p>
-            <button onClick={toggleEmployed}>Toggle Employed status</button> */}
-            {/* <input value={name} onChange={handleNameChange}></input>
-            <p>Name: {name}</p>
-
-            <input value={quantity} onChange={handleQuantityChange} type="number"></input>
-            <p>Quantity: {quantity}</p>
-
-            <textarea value={comment} onChange={handleCommentChange} placeholder="Enter Delivery Instructions"></textarea>
-            <p>Comment: {comment}</p>
-
-            <select value={payment} onChange={handlePaymentChange}>
-                <option value={""}>Select an option</option>
-                <option value={"Visa"}>Visa</option>
-                <option value={"Mastercard"}>Mastercard</option>
-                <option value={"Giftcard"}>Giftcard</option>
-            </select>
-            <p>Payment: {payment}</p>
-
-            <label>
-                <input type="radio" value={"Pick up"} checked={shipping==="Pick up"} onChange={handleShippingChange}/>
-                Pick up
-            </label>
-            <label>
-                <input type="radio" value={"Delivery"} checked={shipping==="Delivery"} onChange={handleShippingChange}/>
-                Delivery
-            </label>
-            <p>Shipping: {shipping}</p> */}
-
-            {/* <p>Your favourite car is : {car.year} {car.make} {car.model}</p>
-
-            <input type="number" value={car.year} onChange={handleYearChange}/><br/>
-            <input type="text" value={car.make} onChange={handleMakeChange}/><br/>
-            <input type="text" value={car.model} onChange={handleModelChange}/><br/> */}
-
-            {/* <h2>List of food</h2>
-            <ul>
-                {foods.map((food, index) => <li key={index} onClick={() => handleRemoveFood(index)}>{food}</li>)}
-            </ul>
-
-            <input type="text" id="foodInput" placeholder="Enter food name"/>
-            <button onClick={handleAddFood}>Add food</button> */}
-
-            <h2>List of Car Objects</h2>
-            <ul>
-                {cars.map((car, index) => 
-                <li key={index} onClick={() => handleRemoveCar(index)}>
-                    {car.year} {car.make} {car.model}
-                </li>)}
-            </ul>
-
-            <input type="number" value={carYear} onChange={handleYearChange}/><br />
-            <input type="text" value={carMake} onChange={handleMakeChange} placeholder="Enter car make"/><br />
-            <input type="text" value={carModel} onChange={handleModelChange} placeholder="Enter car model"/><br />
-            <button onClick={handleAddCar}>Add Car</button>
-
-        </div>
-    );
-    
-
+        <>
+            <p>Count: {count}</p>
+            <button onClick={addCount}>Add</button>
+        </>
+    )
 }
 
 export default MyComponent
